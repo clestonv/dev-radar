b0d9a9fd5b2537df59297f3420abfab5a14cd9a6
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-04-18 12:56:38 -0300

    First Commit

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArrays');
+
+module.exports = {
+    async index(request, response) {
+        //Buscar todos dev num raio de 10KM
+        //Filtrar por tecnologia
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs:{
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000
+                },
+            }
+        });
+
+        return response.json({devs})
+    }
+}
